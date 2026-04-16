01d16811a2fc1f18f2b2f176a2ae2b01053289b1
yofiyamariraja@gmail.com <yofiyamariraja@gmail.com>
2026-04-16 12:41:52 +0530

    url update

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,11 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_size=5,
+    max_overflow=5,
+)
 
 def get_session():
     with Session(engine) as session:
